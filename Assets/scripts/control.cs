e58c26e713ca43a6385bee224940c4d11bf13016
Marcus Williams <marcusw633@gmail.com>
2017-08-02 16:26:29 -0400

    DropOff init files

---
+++ Assets/scripts/control.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class control : MonoBehaviour {
+	
+	Rigidbody rb;
+	public float speed;
+	public float turn;
+	public float shift;
+
+	private bool hitR = false;
+	private bool hitL = false;
+	private bool drift = false;
+
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		FixedUpdate();
+
+		if (hitL == false) {
+			if (Input.GetKey (KeyCode.LeftArrow))
+			{
+				transform.position -= transform.right * shift * Time.deltaTime;
+//				if (drift == true) {
+//					transform.RotateAround(transform.position, transform.up, -turn);
+//				}
+//				else {
+//					transform.position -= transform.right * shift * Time.deltaTime;
+//				}
+			}
+		}
+		if (hitR == false) {
+			if (Input.GetKey (KeyCode.RightArrow))
+			{
+				transform.position += transform.right * shift * Time.deltaTime;
+//				if (drift == true) {
+//					transform.RotateAround(transform.position, transform.up, turn);
+//				}
+//				else {
+//					transform.position += transform.right * shift * Time.deltaTime;
+//				}
+			}
+		}
+
+		if (speed < 10) {
+			speed = 0;
+		}
+	}
+
+	void FixedUpdate () {
+
+		rb.AddForce(transform.forward * speed, ForceMode.Acceleration);
+		if (drift == true) {
+//			if (Input.GetKey (KeyCode.UpArrow)) {
+//				transform.position += transform.forward * speed/2f * Time.deltaTime;
+//			}
+			speed -= .001f;
+		}
+
+	}
+	
+	void OnCollisionEnter (Collision col) {
+		if (col.collider.tag == "wallR") {
+			hitR = true;
+		}
+		if (col.collider.tag == "wallL") {
+			hitL = true;
+		}
+
+	}
+
+	void OnCollisionStay (Collision col) {
+		if (col.collider.tag == "wallR") {
+			hitR = true;
+		}
+		if (col.collider.tag == "wallL") {
+			hitL = true;
+		}
+	}
+
+	void OnCollisionExit (Collision col) {
+		if (col.collider.tag == "wallR") {
+			hitR = false;
+		}
+		if (col.collider.tag == "wallL") {
+			hitL = false;
+		}
+	}
+
+	void OnTriggerEnter(Collider col){
+		if (col.GetComponent<Collider> ().tag == "DropOff") {
+			drift = true;
+			//speed = speed/2f;
+			//rb.angularDrag = .5f;
+			//rb.drag = 1f;
+		}
+		if (col.GetComponent<Collider> ().tag == "SpdBump") {
+			speed -= 2.5f;
+			rb.angularDrag += .01f;
+		}
+	}
+	void OnTriggerStay(Collider col){
+		if (col.GetComponent<Collider> ().tag == "OffRoad") {
+			speed -= 1f;
+			rb.angularDrag += .01f;
+		}
+		if (col.GetComponent<Collider> ().tag == "wall") {
+			speed = 0f;
+			rb.angularDrag += 1f;
+			rb.drag = 1f;
+		}
+	}
+}
---
+++ Assets/scripts/speed.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class speed : MonoBehaviour {
+
+	public Text t;
+	public Text d;
+	public GameObject c;
+
+	private string s;
+	private string x;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		s = "Speed: " + c.GetComponent<control> ().speed.ToString ();
+		t.text = s;
+
+		x = "Distance: " + Mathf.Abs(c.transform.position.z) % 637;
+		d.text = x;
+	}
+}
